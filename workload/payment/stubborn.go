0c2c3a7d71f63bcecca750cdd0876bcadb7cfc56
Mariem Baccari <mariembaccari70@gmail.com>
2024-12-01 23:06:11 +0100

    add more logs

--- workload/payment/stubborn.go
+++ workload/payment/stubborn.go
@@ -264,7 +264,9 @@ func (s *StubbornPaymentUser) Restart(info behavior.RestartInfo) error {
 		return fmt.Errorf("invalid new stubbornPaymentUser")
 	}
 
+	logging.Debugf("restart signal sending")
 	s.restartCh <- info
+	logging.Debugf("sent restart signal")
 
 	return nil
 }
