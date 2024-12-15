2820f65e8ae64531c0ec2284a379f479683db910
Mariem Baccari <mariembaccari70@gmail.com>
2024-12-15 22:27:00 +0100

    try with 1 user

--- workload/store/application.go
+++ workload/store/application.go
@@ -4,6 +4,7 @@ import (
 	"diablo/blockchain"
 	"diablo/core/logging"
 	"fmt"
+	"github.com/ethereum/go-ethereum/common"
 	"os"
 	"time"
 )
@@ -48,7 +49,7 @@ func (s *Application) SetItem(key, value string, timeout time.Duration) error {
 
 	logging.Infof("calling set with keybytes: %s, valuebytes: %s", string(keyBytes[:]), string(valueBytes[:]))
 
-	return s.client.SendContractTransaction(s.contractAddress, s.abi, "setItem", timeout, keyBytes, valueBytes)
+	return s.client.SendContractTransaction(s.contractAddress, s.abi, "setItem", timeout, common.LeftPadBytes([]byte(key), 32), common.LeftPadBytes([]byte(value), 32))
 }
 
 func (s *Application) GetItem(key string, timeout time.Duration) (string, error) {
@@ -57,7 +58,7 @@ func (s *Application) GetItem(key string, timeout time.Duration) (string, error)
 
 	var result [32]byte
 	logging.Infof("calling items on keybytes: %s", string(keyBytes[:]))
-	err := s.client.CallContract(s.contractAddress, s.abi, "items", timeout, &result, keyBytes)
+	err := s.client.CallContract(s.contractAddress, s.abi, "items", timeout, &result, common.LeftPadBytes([]byte(key), 32))
 	if err != nil {
 		return "", fmt.Errorf("failed to get item %s: %w", key, err)
 	}
