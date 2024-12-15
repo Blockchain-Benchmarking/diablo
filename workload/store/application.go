3a5e4d15cff0d6cc03ccfe23b7c2b1ac43c784dc
Mariem Baccari <mariembaccari70@gmail.com>
2024-12-15 18:14:46 +0100

    try raw call

--- workload/store/application.go
+++ workload/store/application.go
@@ -54,13 +54,7 @@ func (s *Application) GetItem(key string) (string, error) {
 		return "", fmt.Errorf("failed to get item %s: %w", key, err)
 	}
 
-	value, ok := result.([32]byte)
-	if !ok {
-		logging.Warnf("item %s is not a byte array", key)
-		return "", fmt.Errorf("failed to get item %s: %w", key, fmt.Errorf("result is not of type string"))
-	}
-
-	str := fmt.Sprintf("%x", value)
+	str := result.(string)
 	logging.Infof("got item %s", str)
 
 	return str, nil
