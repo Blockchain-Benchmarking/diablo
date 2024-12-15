77de2fa01f7e5bc1df6d23c449facb3a5819b0eb
Mariem Baccari <mariembaccari70@gmail.com>
2024-12-15 15:47:02 +0100

    add debug log statements

--- workload/store/application.go
+++ workload/store/application.go
@@ -46,7 +46,7 @@ func (s *Application) SetItem(key, value string, timeout time.Duration) error {
 
 func (s *Application) GetItem(key string) (string, error) {
 	logging.Infof("in getitem")
-	result, err := s.client.CallContract(s.contractAddress, s.abi, "items", common.LeftPadBytes([]byte(key), 32))
+	result, err := s.client.CallContract(s.contractAddress, s.abi, "items", common.BytesToHash([]byte(key)))
 	if err != nil {
 		return "", fmt.Errorf("failed to get item %s: %w", key, err)
 	}
